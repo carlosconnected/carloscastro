e84cb8153f6ee8a0ef098046f10aae443ca561c8
Carlos Castro <carloz.castro@gmail.com>
2026-01-22 14:56:15 -0500

    Enhance animations in globals.css and add starfield background to SiteLayout component

--- components/SiteLayout.tsx
+++ components/SiteLayout.tsx
@@ -4,7 +4,7 @@ import Navbar from "./Navbar";
 
 export default function SiteLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="min-h-screen flex flex-col bg-neutral-50">
+    <div className="min-h-screen flex flex-col bg-black starfield">
       <Navbar />
 
       <main className="flex-1 flex items-start justify-center px-4 py-10">
