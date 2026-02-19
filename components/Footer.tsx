e8c21d5d1fd2ffeabe35bcfada31b3dbe59c585c
Carlos Castro <carloz.castro@gmail.com>
2026-02-19 14:12:28 -0500

    Make my name consistent

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-neutral-900 text-white">
       <div className="mx-auto max-w-6xl px-4 py-4 text-center text-sm">
-        © {new Date().getFullYear()} Carlos Castro. All rights reserved
+        © {new Date().getFullYear()} Carlos Castro Vargas. All rights reserved
       </div>
     </footer>
   );
